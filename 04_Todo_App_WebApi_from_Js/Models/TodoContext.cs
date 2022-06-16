eebb4e7babf935b909307b9415990495ae115b22
Spencer Thomason <spencer.thomason@gmail.com>
2022-06-16 18:50:36 -0400

    Todo Api Accessed from Javascript using fetch api

---
+++ 04_Todo_App_WebApi_from_Js/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _04_Todo_App_WebApi_from_Js.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ 04_Todo_App_WebApi_from_Js/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace _04_Todo_App_WebApi_from_Js.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
