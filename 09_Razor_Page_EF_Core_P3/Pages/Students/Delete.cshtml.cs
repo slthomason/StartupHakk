0274e6819abfa733c4b91ccabf4d340a6362d459
Spencer Thomason <spencer.thomason@gmail.com>
2022-06-27 16:02:26 -0400

    razor pages with EF core part 3

---
+++ 09_Razor_Page_EF_Core_P3/Pages/Students/Delete.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using _09_Razor_Page_EF_Core_P3.Data;
+using _09_Razor_Page_EF_Core_P3.Models;
+
+namespace _09_Razor_Page_EF_Core_P3.Pages.Students
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly _09_Razor_Page_EF_Core_P3.Data.SchoolContext _context;
+
+        public DeleteModel(_09_Razor_Page_EF_Core_P3.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Student Student { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ErrorMessage = String.Format("Delete {ID} failed. Try again", id);
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                return RedirectToAction("./Delete",
+                                     new { id, saveChangesError = true });
+            }
+        }
+    }
+}
