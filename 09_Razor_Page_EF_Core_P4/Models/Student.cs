8a0a0a29f47e71ad7c9e50837be6e911e0e923f2
Spencer Thomason <spencer.thomason@gmail.com>
2022-06-28 20:46:46 -0400

    razor pages ef core p4 migration added

---
+++ 09_Razor_Page_EF_Core_P4/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace _09_Razor_Page_EF_Core_P4.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Secret { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+       
+    }
+}
---
+++ 09_Razor_Page_EF_Core_P4/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using _09_Razor_Page_EF_Core_P4.Data;
+using _09_Razor_Page_EF_Core_P4.Models;
+using _09_Razor_Page_EF_Core_P4.ViewModels;
+
+namespace _09_Razor_Page_EF_Core_P4.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly _09_Razor_Page_EF_Core_P4.Data.SchoolContext _context;
+
+        public CreateModel(_09_Razor_Page_EF_Core_P4.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public StudentVM StudentVM { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var emptyStudent = new Student();
+
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                var entry = _context.Add(emptyStudent);
+                entry.CurrentValues.SetValues(StudentVM);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+    }
+}
