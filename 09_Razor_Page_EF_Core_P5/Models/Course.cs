06fac59361863a90c7bdf912a41c7f8a2cef2294
Spencer Thomason <spencer.thomason@gmail.com>
2022-06-29 21:38:09 -0400

    added complex structure and mirgration

---
+++ 09_Razor_Page_EF_Core_P5/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _09_Razor_Page_EF_Core_P5.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Range(0, 5)]
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Instructor> Instructors { get; set; }
+    }
+}
---
+++ 09_Razor_Page_EF_Core_P5/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using _09_Razor_Page_EF_Core_P5.Data;
+using _09_Razor_Page_EF_Core_P5.Models;
+
+namespace _09_Razor_Page_EF_Core_P5.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly _09_Razor_Page_EF_Core_P5.Data.SchoolContext _context;
+
+        public DetailsModel(_09_Razor_Page_EF_Core_P5.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                                        .Include(s => s.Enrollments)
+                                        .ThenInclude(e => e.Course)
+                                        .AsNoTracking()
+                                        .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
