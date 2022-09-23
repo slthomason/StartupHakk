88921350fd9d03ec339fa56082bb3996f7dfae07
Spencer Thomason <spencer.thomason@gmail.com>
2022-09-23 19:21:08 -0400

    xml output in minimal api

---
+++ 10_Mininum_WebApi_Xml_Output/Approach2.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Serialization;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace _10_Mininum_WebApi_Xml_Output
+{
+
+    public class Approach2<T> : IResult
+    {
+        private static readonly XmlSerializer Serializer = new(typeof(T));
+        private readonly T _result;
+
+        public Approach2(T result)
+        {
+            _result = result;
+        }
+
+        public async Task ExecuteAsync(HttpContext httpContext)
+        {
+            // 👇Create a FileBufferingWriteStream instead of a MemoryStream here
+            using var ms = new FileBufferingWriteStream();
+            Serializer.Serialize(ms, _result);
+
+            httpContext.Response.ContentType = "application/xml";
+            await ms.DrainBufferAsync(httpContext.Response.Body);
+            // 👆 Call DrainBufferAsync instead of CopyToAsync, and don't call ms.Position = 0
+        }
+    }
+}
---
+++ 10_Mininum_WebApi_Xml_Output/Approach3.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Serialization;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace _10_Mininum_WebApi_Xml_Output
+{
+    public static class StreamManager
+    {
+        // 👇 Create a shared RecyclableMemoryStreamManager instance
+        public static readonly Microsoft.IO.RecyclableMemoryStreamManager Instance = new();
+    }
+    public class Approach3<T> : IResult
+    {
+        private static readonly XmlSerializer Serializer = new(typeof(T));
+        private readonly T _result;
+
+        public Approach3(T result)
+        {
+            _result = result;
+        }
+
+        public async Task ExecuteAsync(HttpContext httpContext)
+        {
+            // 👇Use in place of the MemoryStream here
+            using var ms = StreamManager.Instance.GetStream();
+            Serializer.Serialize(ms, _result);
+
+            httpContext.Response.ContentType = "application/xml";
+            ms.Position = 0;
+            await ms.CopyToAsync(httpContext.Response.Body);
+        }
+    }
+}
