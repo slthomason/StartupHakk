03ccff9d78646ce807579a2cbf5ce8a41b7c758b
hamzastartuphakk <hamza@startuphakk.com>
2023-07-20 02:16:55 +0500

    Real time chat messages with SignalR

---
+++ 48_Real_Time_Chat_with_SignalR_DotNet/DashboarHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication6
+{
+    public class DashboardHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
