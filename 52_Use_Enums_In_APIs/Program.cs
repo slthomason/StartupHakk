47fb1e0d0f991abef4523f32b02c599f266fd9f0
hamzastartuphakk <hamza@startuphakk.com>
2023-07-25 23:20:24 +0500

    Use Enums in APIs

---
+++ 52_Use_Enums_In_APIs/Program.cs
@@ -0,0 +1,51 @@
+public enum MyEnum {
+     @JsonProperty("theFirstValue") THE_FIRST_VALUE,
+     @JsonProperty("another_value") ANOTHER_VALUE;
+     // another one here, or delete the previous ones
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public enum AddressType { 
+    HOME(true),
+    WORK(false),
+    OTHER(true);
+    private AddressType(boolean businessLogicCondition) {
+        this.businessLogicCondition = businessLogicCondition;
+    }
+    private final boolean businessLogicCondition;
+    public boolean getCond() {
+        return businessLogicCondition;
+    }
+};
+@Entity
+public class Person {
+@Enumerated
+private AddressType address;
+// ...
+}
+// you can use it after inflation in your code where needed 
+Person p1 = service.getPersonById("123");
+if(p1.getAddress().getCond()) { /*...*/ }
