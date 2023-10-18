9d606a0dfd99bc5ebfa748cbe9cc0606f2158b28
hamzastartuphakk <hamza@startuphakk.com>
2023-10-19 02:35:37 +0500

    Create a Windows Service App in .NET Core

---
+++ 103_Windows_Service_In_DotNetCore/WindowsServiceDemo/Demo/Services/ServiceB.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo.Services
+{
+    public interface IServiceB
+    {
+        void Run();
+    }
+
+    public class ServiceB : IServiceB
+    {
+        private readonly ILogger<ServiceB> _logger;
+
+        public ServiceB(ILogger<ServiceB> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            _logger.LogInformation("In Service B");
+        }
+    }
+}
